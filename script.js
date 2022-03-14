436e6ba5e92627bc1a8b1d57fabc11399ce6251d
Victor_Nov <victornov@bk.ru>
2022-03-14 23:43:40 +0500

    Добавлено модальное меню в мобильной версии

--- script.js
+++ script.js
@@ -1,10 +1,22 @@
+let burgerButton = document.querySelector(".burger-menu-button");
+let burgerIcon = document.querySelector(".burger-menu-icon");
+let modalMenu = document.querySelector(".header-menu-modal");
+let modalMenuButtons = document.querySelectorAll(".menu-modal-item");
+
+burgerButton.addEventListener( "click", () => {
+    burgerIcon.classList.toggle("active");
+    modalMenu.classList.toggle("open");
+} );
+modalMenuButtons.forEach( (item) => item.addEventListener( "click", () => {
+    burgerIcon.classList.remove("active");
+    modalMenu.classList.remove("open");
+} ) );
+
 let contactButton = document.querySelector(".contacts-button");
 let closeContactButton = document.querySelector(".modal-contacts .icon-close");
-//let modalBackground = document.querySelector(".modal-contacts-container");
 let submitButton = document.querySelector(".contacts-form input[type='submit']");
 let modalContacts = document.querySelector(".modal-contacts-container");
 
 contactButton.addEventListener( "click", () => modalContacts.classList.add("open") );
 closeContactButton.addEventListener( "click", () => modalContacts.classList.remove("open") );
 submitButton.addEventListener( "click", () => modalContacts.classList.remove("open") );
-//modalBackground.addEventListener( "click", () => modalContacts.classList.remove("open") );
